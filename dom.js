f780dc36ff18413eceabef7535967469c862f576
Ashu-Bashir <ashu.bashir@anywhere.co>
2023-02-17 15:20:12 +0530

    Fixed the major bug of sending multiple requests , when previous one was deleted and a new one was created.

--- dom.js
+++ dom.js
@@ -6,6 +6,10 @@ const bodyInput = document.querySelector("#body");
 const heading = document.querySelector("#headingResult");
 const viewPostList = document.querySelector("#viewPostList");
 const titleHeading = document.querySelector("#titleHeading");
+const editBtn = document.querySelector("#editBtn");
+const deleteBtn = document.querySelector("#deleteBtn");
+const submitBtn = document.querySelector("#submitPost");
+let data;
 
 const hidePostMessage = () => {
     setTimeout(() => {
@@ -14,7 +18,7 @@ const hidePostMessage = () => {
     }, 3000); 
   }
 
-//   viewPostBtn.disabled= true;
+//   viewPostBtn.disabled= true;  
 viewPostBtn.addEventListener("click" , ()=>{
         heading.innerHTML =`<h4><i>Please Add The Log By Clicking Publish Button</i></h4>`;  
         viewPostBtn.disabled= true;
@@ -26,9 +30,10 @@ viewPostBtn.addEventListener("click" , ()=>{
 publishPostBtn.addEventListener("click" , (e)=>{
     e.preventDefault();
     publishPostInputs.hidden = false;
-    const submitBtn = document.querySelector("#submitPost");
+});
 
-   submitBtn.addEventListener("click" , async (e)=>{
+   // ADDING SUBMIT BTN EVENT LISTENER
+    submitBtn.addEventListener("click" , async (e)=>{
         e.preventDefault();
         publishPostBtn.disabled =  true;
         if (titleInput.value === "" && bodyInput.value === "") {
@@ -50,110 +55,104 @@ publishPostBtn.addEventListener("click" , (e)=>{
         if (!response.ok) {
             throw new Error("Network response was not ok");
           }
-          
-          const data = await response.json();
+          data = await response.json();
           heading.innerHTML = `<h4><i>Request was successful</i></h4>`
           publishPostInputs.hidden = true;
           viewPostBtn.disabled = true 
-          hidePostMessage();
+          hidePostMessage();        
+        } 
+        catch (error) {
+            console.error(error);
+        }
+});
 
-          // ADDING VIEW POST BUTTON
-           viewPostBtn.addEventListener("click" , (e)=>{
+// Adding all the logic outside Submit event listener to prevent attaching multiple event listeners to each.
+
+ // ADDING VIEW POST BUTTON
+        viewPostBtn.addEventListener("click" , (e)=>{
+            e.preventDefault();
+            viewPostList.hidden = false;
+            heading.innerHTML= "" 
+            titleHeading.innerHTML = `The Title is <i><b>${data.title}</b></i>`
+        });
+            
+  //  //ADDING DELETE BUTTON TO DELETE THE LOG
+          const hideDeleteMessage = () => {
+              setTimeout(() => {
+              heading.innerHTML = "";
+              },1000); 
+          }
+          deleteBtn.addEventListener("click", async (e) => {
+              e.preventDefault();                
+              try {
+                  deleteBtn.disabled = true;
+                  const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${data.id}`,{
+                  method: "DELETE",
+                  }
+              );
+          
+              if (response.ok) {
+                  heading.innerHTML =`<h4><i>The Log Is Deleted.</i></h4>`;
+                  viewPostList.hidden = true;
+                  submitBtn.disabled= false;
+                  submitBtn.hidden = false
+                  titleInput.value= "";
+                  bodyInput.value = "";
+                  deleteBtn.disabled = false;
+                  viewPostBtn.hidden = false;
+                  publishPostBtn.disabled= false
+                  hideDeleteMessage();
+              }
+              } catch (error) {
+              console.error( error);
+              }
+          });
+
+
+          // ADDING EDIT BUTTON TO EDIT THE DATA
+          editBtn.addEventListener("click" , (e)=>{
+              const saveBtn = document.querySelector("#saveBtn");
               e.preventDefault();
-              viewPostList.hidden = false;
-              heading.innerHTML= "" 
-              titleHeading.innerHTML = `The Title is <i><b>${data.title}</b></i>`
-            });
-               const editBtn = document.querySelector("#editBtn");
-               const deleteBtn = document.querySelector("#deleteBtn");
-                  
-               //ADDING DELETE BUTTON TO DELETE THE LOG
-                    const hideDeleteMessage = () => {
-                        setTimeout(() => {
-                        heading.innerHTML = "";
-                        }, 3000); 
-                    }
-                    deleteBtn.addEventListener("click", async (e) => {
-                        e.preventDefault();                
-                        try {
-                            deleteBtn.disabled = true;
-                        const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${data.id}`,{
-                            method: "DELETE",
-                            }
-                        );
-                    
-                        if (response.ok) {
-                            heading.innerHTML =`<h4><i>The Log Is Deleted.</i></h4>`;
-                            viewPostList.hidden = true;
-                            submitBtn.disabled= false;
-                            titleInput.value= "";
-                            bodyInput.value = "";
-                            hideDeleteMessage();
-                            deleteBtn.disabled = false;
-                            viewPostBtn.hidden = false;
-                            publishPostBtn.disabled= false
-                            submitBtn.hidden = false
+              publishPostBtn.hidden= false;
+              titleInput.value = data.title;
+              bodyInput.value = data.body;
+              publishPostInputs.hidden = false;
+              submitBtn.hidden = true;
+              viewPostBtn.hidden = false;
+              viewPostBtn.disabled= true;
+              viewPostList.hidden = true;
+              saveBtn.hidden = false;
+              saveBtn.disabled= false;
 
+          });
 
-                        }
-                        } catch (error) {
-                        console.error( error);
-                        }
-                    });
-
-
-                    // ADDING EDIT BUTTON TO EDIT THE DATA
-                    editBtn.addEventListener("click" , (e)=>{
-                        const saveBtn = document.querySelector("#saveBtn");
-                        e.preventDefault();
-                        publishPostBtn.hidden= false;
-                        titleInput.value = data.title;
-                        bodyInput.value = data.body;
-                        publishPostInputs.hidden = false;
-                        submitBtn.hidden = true;
-                        viewPostBtn.hidden = false;
-                        viewPostBtn.disabled= true;
-                        viewPostList.hidden = true;
-                        saveBtn.hidden = false;
-                        saveBtn.disabled= false;
-
-                    });
-                        saveBtn.addEventListener("click" , async(e)=>{
-                            e.preventDefault();
-                            try {
-                                saveBtn.disabled = true;
-                                const response = await fetch(`https://jsonplaceholder.typicode.com/posts/1` , {
-                                    method: "PUT",
-                                    headers: {
-                                    "Content-type": "application/json; charset=UTF-8",
-                                    },
-                                    body: JSON.stringify({
-                                    title: titleInput.value,
-                                    body: bodyInput.value,
-                                    }),
-                                });
-                                const updatedData = await response.json();
-                                viewPostList.hidden = false;
-                                titleHeading.innerHTML = `The Title is <i><b>${updatedData.title}</b></i>`;
-                                saveBtn.hidden= true;
-                                publishPostInputs.hidden = true;
-                                publishPostBtn.hidden= false;
-                                data.title = updatedData.title;
-                                data.body= updatedData.body;
-                                saveBtn.disabled = false
-                                
-                            } catch (error) {
-                                console.error(error);
-                            }
-                        });            
-        
-    } 
-    catch (error) {
-        console.error(error);
-    }
-
-});
-
-});
+              saveBtn.addEventListener("click" , async(e)=>{
+                  e.preventDefault();
+                  try {
+                      saveBtn.disabled = true;
+                      const response = await fetch(`https://jsonplaceholder.typicode.com/posts/1` , {
+                          method: "PUT",
+                          headers: {
+                          "Content-type": "application/json; charset=UTF-8",
+                          },
+                          body: JSON.stringify({
+                          title: titleInput.value,
+                          body: bodyInput.value,
+                          }),
+                      });
+                      const updatedData = await response.json();
+                      viewPostList.hidden = false;
+                      titleHeading.innerHTML = `The Title is <i><b>${updatedData.title}</b></i>`;
+                      saveBtn.hidden= true;
+                      publishPostInputs.hidden = true;
+                      publishPostBtn.hidden= false;
+                      data.title = updatedData.title;
+                      data.body= updatedData.body;
+                      saveBtn.disabled = false
+                      
+                  } catch (error) {
+                      console.error(error);
+                  }
+              }); 
 
 
